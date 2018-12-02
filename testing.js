40cb04a79fb01841368b6185ad953a0ead0143bc
kenokabe <stken2050@gmail.com>
2018-12-02 21:11:23 +0900

    Update

---
+++ testing.js
@@ -0,0 +1,236 @@
+const array1 = [1, 2, 3, 4, 5];
+
+
+const f = array =>
+    array
+        .flatMap(a => [a * 2])
+        .flatMap(a => [a + 1]);
+
+
+
+const array2 =
+    [array1].flatMap(f);
+
+
+console.log(array2);
+
+
+
+
+
+
+/*
+
+
+const array2 = array1
+    .flatMap(a =>
+
+        a % 2 === 1
+            ? [[a]]
+            : [[]]
+    )
+
+
+
+
+
+
+console.log(
+    array1
+        .flatMap(a =>
+
+            a % 2 === 1
+                ? [[a]]
+                : [[]]
+        )
+
+);
+
+
+console.log(
+    array1
+        .map(a =>
+
+            a % 2 === 1
+                ? a
+                : undefined
+        )
+    // .flat()
+
+);
+
+
+
+const unit = a => [a];
+const f = a => [a * 2];
+
+//self structure
+
+console.log(
+
+    unit(3)
+        .flatMap(f)
+
+);
+
+console.log(
+
+    f(3)
+
+);
+
+
+
+console.log(
+
+    [3].flatMap(a => [a])
+
+);
+
+console.log(
+
+    f(3).flatMap(unit)
+
+);
+
+console.log(
+
+    unit(3).flatMap(unit)
+
+); //unit  * unit = unit
+
+
+console.log(
+    "==================="
+);
+{
+    const f = a => [a * 2];
+    const g = a => [a + 1];
+    console.log(
+
+        unit(3)
+    );
+
+    console.log(
+
+        unit(3)
+            .flatMap(f)
+    );
+
+    {
+        console.log(
+
+            unit(3)
+                .flatMap(f)
+                .flatMap(g)
+        );
+
+
+        const h = a => unit(a)
+            .flatMap(f)
+            .flatMap(g);
+
+
+        console.log(
+            unit(3)
+                .flatMap(h)
+        );
+    }
+}
+console.log(
+    "==================="
+);
+{
+    const f = a => a * 2;
+    const g = a => a + 1;
+    console.log(
+
+        unit(3)
+            .map(f)
+            .map(g)
+    );
+
+
+    const h = a => [a]
+        .map(f)
+        .map(g);
+
+
+    console.log(
+        unit(3)
+            .map(h)
+    );
+}
+
+//map implementation differentials
+
+
+
+
+Object.defineProperty(
+    Number.prototype,
+    "plus", {
+        value: function (val) {
+            return this + val;
+        }
+    });
+
+//plus(plus(plus(1)(2))(5))(9)
+
+console.log((1).plus(9))
+
+console.log(
+    "Hello world"
+        .replace("Hello", "Hi")
+        .replace("world", "JavaScript")
+
+);
+
+{
+    const e = Number;
+
+    Object.defineProperty(
+        e.prototype,
+        "map", {
+            value: function (f) {
+                return f(this);
+            }
+        });
+    const f = a => a * 2;
+    console.log(
+        e(1).map(f)
+    );
+
+    console.log(
+        e(1).map(e)
+    );
+
+    console.log(
+        e(1) === 1
+    );
+
+}
+
+const a = {};
+
+
+Object.defineProperty(a,
+    "now", {
+        set: (x) => {
+            console.log(x);
+        }
+    });
+
+
+{
+
+    Object.defineProperty(a,
+        "now", {
+            set: (x) => {
+                console.log(x);
+            }
+        });
+}
+a.now = 999;
+
+*/
