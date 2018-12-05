fc8d34160d3a4647f4834f6f6b2415727b0df57e
kenokabe <stken2050@gmail.com>
2018-12-05 23:53:12 +0900

    Update

--- testing.js
+++ testing.js
@@ -1,253 +1,52 @@
+/*const f = p => p.then(n => n * 2);
 
-const unit = a => [a];
+const a = Promise.resolve(1);
 
-const a = [1, 2];
-const f = a =>
- a.flatMap(a => [a * 10, a * 100]);
-
-const left = unit(a).flatMap(f);
 const center = f(a);
-const right = f(a).flatMap(unit);
-
-console.log(left);
 console.log(center);
-console.log(right);
 
-{
- const f = array =>
-  [array]
-   .flatMap(a => [a * 2])
-   .flatMap(a => [a + 1]);
-
- const array1 = [1, 2, 3, 4, 5].flatMap(f);
-
- console.log(array1);
-}
-{
- const array1 =
-  [1, 2, 3]
-   .flatMap(a => [a * 2])
-   .flatMap(a => [a + 1])
-
- console.log(array1);
-}
-{
- const array1 =
-  [1, 2, 3]
-   .flatMap(a =>
-    [a]
-     .flatMap(a => [a * 2])
-     .flatMap(a => [a + 1])
-   );
-
- console.log(array1);
-}
-
-
-/*
-
-
-
-
-
-
-
-console.log(
-    array1
-        .flatMap(a =>
-
-            a % 2 === 1
-                ? [[a]]
-                : [[]]
-        )
-
-);
-
-
-console.log(
-    array1
-        .map(a =>
-
-            a % 2 === 1
-                ? a
-                : undefined
-        )
-    // .flat()
-
-);
-
-
-
-const unit = a => [a];
-const f = a => [a * 2];
-
-//self structure
-
-console.log(
-
-    unit(3)
-        .flatMap(f)
-
-);
-
-console.log(
-
-    f(3)
-
-);
-
-
-
-console.log(
-
-    [3].flatMap(a => [a])
-
-);
-
-console.log(
-
-    f(3).flatMap(unit)
-
-);
-
-console.log(
-
-    unit(3).flatMap(unit)
-
-); //unit  * unit = unit
-
-
-console.log(
-    "==================="
-);
-{
-    const f = a => [a * 2];
-    const g = a => [a + 1];
-    console.log(
-
-        unit(3)
-    );
-
-    console.log(
-
-        unit(3)
-            .flatMap(f)
-    );
-
-    {
-        console.log(
-
-            unit(3)
-                .flatMap(f)
-                .flatMap(g)
-        );
-
-
-        const h = a => unit(a)
-            .flatMap(f)
-            .flatMap(g);
-
-
-        console.log(
-            unit(3)
-                .flatMap(h)
-        );
-    }
-}
-console.log(
-    "==================="
-);
-{
-    const f = a => a * 2;
-    const g = a => a + 1;
-    console.log(
-
-        unit(3)
-            .map(f)
-            .map(g)
-    );
-
-
-    const h = a => [a]
-        .map(f)
-        .map(g);
-
-
-    console.log(
-        unit(3)
-            .map(h)
-    );
-}
-
-//map implementation differentials
-
-
-
-
-Object.defineProperty(
-    Number.prototype,
-    "plus", {
-        value: function (val) {
-            return this + val;
-        }
-    });
-
-//plus(plus(plus(1)(2))(5))(9)
-
-console.log((1).plus(9))
-
-console.log(
-    "Hello world"
-        .replace("Hello", "Hi")
-        .replace("world", "JavaScript")
-
-);
-
-{
-    const e = Number;
-
-    Object.defineProperty(
-        e.prototype,
-        "map", {
-            value: function (f) {
-                return f(this);
-            }
-        });
-    const f = a => a * 2;
-    console.log(
-        e(1).map(f)
-    );
-
-    console.log(
-        e(1).map(e)
-    );
-
-    console.log(
-        e(1) === 1
-    );
-
-}
-
-const a = {};
-
-
-Object.defineProperty(a,
-    "now", {
-        set: (x) => {
-            console.log(x);
-        }
-    });
-
-
-{
-
-    Object.defineProperty(a,
-        "now", {
-            set: (x) => {
-                console.log(x);
-            }
-        });
-}
-a.now = 999;
-
+const output = Promise.resolve(a).then(f);
 */
+/*{
+ const unit = Promise.resolve.bind(Promise);
+
+ const p = unit(3);
+ console.log(p);
+
+ const f = p => p.then(p => p * 2);
+
+ const center = f(p);
+
+ console.log(center);
+ const left = unit(p).then(f);
+
+}*///
+
+const unit = Promise.resolve.bind(Promise);
+
+const p = unit(3);
+const pp = unit(p);
+const ppp = unit(pp)
+
+console.log(p);
+console.log(pp);
+console.log(ppp);
+
+{
+ const unit = Number;
+
+ const n = unit(1);
+ const nn = unit(n);
+
+ console.log(n);
+ console.log(nn);
+}
+{
+ const unit = String;
+
+ const s = unit("Hello");
+ const ss = unit(s);
+
+ console.log(s);
+ console.log(ss);
+}
