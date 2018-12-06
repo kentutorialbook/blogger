701e37c4a1e24a1dfcee6891ead03ba2c906aa9c
kenokabe <stken2050@gmail.com>
2018-12-07 06:36:44 +0900

    Update

--- testing.js
+++ testing.js
@@ -5,18 +5,13 @@ const unit = a => [a];
 
 
 
-const plus9 = a =>
-    unit(a)
-        .flatMap(n => unit(n + 9));
+const plus9 = n => unit(n + 9);
 
 
-const oddFilter = a =>
-    unit(a)
-        .flatMap(n =>
-            n % 2 === 1
-                ? unit(n)
-                : []
-        );
+const oddFilter = n =>
+    n % 2 === 1
+        ? unit(n)
+        : [];
 
 const array1 = [1, 2, 3, 4, 5];
 
@@ -34,3 +29,13 @@ const array4 = array1
 
 console.log(array4);
 
+const oddFilter = a =>
+    a % 2 === 1
+        ? unit(a)
+        : [];
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = array1
+    .flatMap(oddFilter);
+
+console.log(array2);
