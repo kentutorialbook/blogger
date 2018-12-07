459a7a5874e3dd78b34093eea86929068f4df611
kenokabe <stken2050@gmail.com>
2018-12-07 13:02:29 +0900

    Update

--- testing.js
+++ testing.js
@@ -1,41 +1,13 @@
-const unit = a => [a];
+const unit = Array.of;
 
+const a = [1, 2];
+const f = a =>
+    a.flatMap(a => [a, a * 5]);
 
+const left = unit(a).flatMap(f);
+const center = f(a);
+const right = f(a).flatMap(unit);
 
-
-
-
-const plus9 = n => unit(n + 9);
-
-
-const oddFilter = n =>
-    n % 2 === 1
-        ? unit(n)
-        : [];
-
-const array1 = [1, 2, 3, 4, 5];
-
-const array3 = array1
-    .flatMap(plus9)
-    .flatMap(oddFilter);
-
-const plus9oddFilter = a =>
-    unit(a)
-        .flatMap(plus9)
-        .flatMap(oddFilter);
-
-const array4 = array1
-    .flatMap(plus9oddFilter);
-
-console.log(array4);
-
-const oddFilter = a =>
-    a % 2 === 1
-        ? unit(a)
-        : [];
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = array1
-    .flatMap(oddFilter);
-
-console.log(array2);
+console.log(left);
+console.log(center);
+console.log(right);
